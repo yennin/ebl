6a46bedda2cdf99b8f1bdcaf65389cca72cb4f27
Patrick Rüegg <patsch@patsch.info>
2016-09-07 21:02:14 +0200

    Added Swipe actions to Recyclerview

--- android/EyotaBookLibrary/app/src/main/java/info/patsch/ebl/books/BookViewFragment.java
+++ android/EyotaBookLibrary/app/src/main/java/info/patsch/ebl/books/BookViewFragment.java
@@ -1,12 +1,19 @@
 package info.patsch.ebl.books;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.v7.util.SortedList;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -41,6 +48,8 @@ public class BookViewFragment extends RecyclerViewFragment implements FilterCons
     private static final String STATE_QUERY = "state_query";
 
     private final static String ARG_FLAGS = "flags";
+
+    private Paint p = new Paint();
 
     private SortedList<Book> model = null;
     private BookAdapter adapter = null;
@@ -88,6 +97,8 @@ public class BookViewFragment extends RecyclerViewFragment implements FilterCons
 
         setLayoutManager(new LinearLayoutManager(getActivity()));
         setAdapter(adapter);
+
+        initSwipe();
     }
 
     @Override
@@ -409,6 +420,60 @@ public class BookViewFragment extends RecyclerViewFragment implements FilterCons
             adapter.getFilter().filter("");
         }
     }
+
+    private void initSwipe() {
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                Book book = (Book)viewHolder.itemView.getTag();
+
+                if (direction == ItemTouchHelper.LEFT) {
+                    // call remove event
+                } else {
+                    editBookInfo(book);
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                    float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                Bitmap icon;
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+
+                    View itemView = viewHolder.itemView;
+                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                    float width = height / 3;
+
+                    if (dX > 0) {
+                        p.setColor(Color.parseColor("#388E3C"));
+                        RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX, (float) itemView.getBottom());
+                        c.drawRect(background, p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_lead_pencil_white_24dp);
+                        RectF icon_dest = new RectF((float) itemView.getLeft() + width, (float) itemView.getTop() + width, (float) itemView.getLeft() + 2 * width, (float) itemView.getBottom() - width);
+                        c.drawBitmap(icon, null, icon_dest, p);
+                    } else {
+                        p.setColor(Color.parseColor("#D32F2F"));
+                        RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
+                        c.drawRect(background, p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_delete_white_24dp);
+                        RectF icon_dest = new RectF((float) itemView.getRight() - 2 * width, (float) itemView.getTop() + width, (float) itemView.getRight() - width, (float) itemView.getBottom() - width);
+                        c.drawBitmap(icon, null, icon_dest, p);
+                    }
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(getRecyclerView());
+    }
+
 
     class BookFilter {
         private String query;
