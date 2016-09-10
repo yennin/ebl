ad4be55a5385d2f19209d7140a8d40cf988253f2
Patrick Rüegg <patsch@patsch.info>
2016-09-10 08:02:00 +0200

    Search dialog refinements

--- android/EyotaBookLibrary/app/src/main/java/info/patsch/ebl/books/search/SearchEntryDialogFragment.java
+++ android/EyotaBookLibrary/app/src/main/java/info/patsch/ebl/books/search/SearchEntryDialogFragment.java
@@ -6,14 +6,20 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.text.Editable;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import info.patsch.ebl.R;
 
 public class SearchEntryDialogFragment extends DialogFragment implements DialogInterface.OnClickListener {
 
     private View form = null;
+    private EditText searchQueryEntry = null;
     private OnDialogClosedListener mListener = null;
 
     @NonNull
@@ -22,11 +28,31 @@ public class SearchEntryDialogFragment extends DialogFragment implements DialogI
 
         form = getActivity().getLayoutInflater().inflate(R.layout.dialog_search, null);
 
+        searchQueryEntry = (EditText)form.findViewById(R.id.search_query);
+
+        searchQueryEntry.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    dismiss();
+                    performSearch();
+                    return true;
+                }
+                return false;
+            }
+        });
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         return builder.setTitle(R.string.search).setView(form)
                 .setPositiveButton(R.string.search, this)
                 .setNegativeButton(android.R.string.cancel, null).create();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
 
     public void setOnDialogClosedListener(OnDialogClosedListener listener) {
@@ -35,10 +61,13 @@ public class SearchEntryDialogFragment extends DialogFragment implements DialogI
 
     @Override
     public void onClick(DialogInterface dialogInterface, int i) {
-        // search
+        performSearch();
+    }
 
-        String query = ((EditText) form.findViewById(R.id.search_query)).getText().toString();
-        if (mListener != null) {
+    private void performSearch() {
+        Editable searchQueryEntryText = searchQueryEntry.getText();
+        String query = searchQueryEntryText != null ? searchQueryEntryText.toString() : null;
+        if (query != null && mListener != null) {
             mListener.onSearchClicked(query);
         }
     }
